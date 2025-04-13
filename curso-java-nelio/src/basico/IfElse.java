f988db58476448f6a9e6cb8bcab993ae46061f58
Ariel Pellegrino <ariel.pell@gmail.com>
2025-04-13 15:04:10 -0300

    aulas
    subindo arquivos de aulas

---
+++ curso-java-nelio/src/basico/IfElse.java
@@ -0,0 +1,26 @@
+package basico;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int x = 5;
+        int hora;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantas horas?");
+        hora = sc.nextInt();
+
+        if (hora < 12){
+            System.out.println("bom dia");
+        } else {
+            if(hora < 18){
+                System.out.println("Boa tarde");
+            } else {
+                System.out.println("Boa noite");
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ curso-java-nelio/src/basico/ProcessamentoDadosCasting.java
@@ -0,0 +1,15 @@
+package basico;
+
+
+public class ProcessamentoDadosCasting {
+    public static void main(String[] args) {
+        int x;
+        double y;
+        x = 5;
+        y = 10;
+
+        y = 3 * x;
+        System.out.println(y);
+        System.out.println(x);
+    }
+}
