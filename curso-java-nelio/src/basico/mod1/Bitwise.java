974b7f61d6596701e0a5efbd14145282ff3b821a
Ariel Pellegrino <ariel.pell@gmail.com>
2025-04-13 17:47:21 -0300

    update Bitwise

--- curso-java-nelio/src/basico/mod1/Bitwise.java
+++ curso-java-nelio/src/basico/mod1/Bitwise.java
@@ -1,15 +1,13 @@
-//package mod1;
-/*
-package basico;
+package mod1;
 import java.util.Scanner;
 
 public class Bitwise {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int mask = 0b0010;   //É possivel inserir a marcação de um numbero binário
+        int mask = 0b00100000;   //É possivel inserir a marcação de um numero binário
 
-        if() {
+        if((n & mask) != 0){ // O operador & compara os bits de dois números
             System.out.printf("6th bit é verdadeiro");
         } else {
             System.out.printf("6th bit é falso");
@@ -19,4 +17,4 @@ public class Bitwise {
     }
 }
 
- */
+
